0f83a73ad1d00c582607520d235f5f577f26284b
Anupam00777 <110917142+Anupam00777@users.noreply.github.com>
2022-08-18 19:27:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -403,7 +403,7 @@ window.addEventListener("keyup", function (event) {
   playerBody.velocity.setZero();
 });
 document.addEventListener("click", function () {
-  document.documentElement.requestFullscreen();
+  // document.documentElement.requestFullscreen();
   if (play == true) {
     document.body.requestPointerLock();
   }
